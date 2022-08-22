e110bac72a058eed1f001c30e00c81df0c69ee52
ErreZac <raphael.zaccone@unige.it>
2022-08-22 15:18:36 +0200

    indent and some tweaks to treesitter conf, stole them from kickstart.nvim

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -13,14 +13,14 @@ require('packer').startup(function(use)
     })
 
     use 'mbbill/undotree'
- 
+
     use ({ 'folke/zen-mode.nvim',
         config = function()
             require("zen-mode").setup {
                 window = {
-                width = .65 -- width will be 85% of the editor width
+                    width = .65 -- width will be 85% of the editor width
+                }
             }
-        }
         end,
     })
 
@@ -56,30 +56,79 @@ require('packer').startup(function(use)
         end
     })
 
+    use 'nvim-treesitter/nvim-treesitter-textobjects'     
     use ({ 'nvim-treesitter/nvim-treesitter',
         config = function()
             require('nvim-treesitter.configs').setup {
                 ensure_installed = {"lua", "c", "bash", "latex", "cpp"}, 
-                highlight = { 
+                highlight = { enable = true },
+                indent = { enable = true },
+                incremental_selection = {
                     enable = true,
+                    keymaps = {
+                        init_selection = '<c-space>',
+                        node_incremental = '<c-space>',
+                        -- TODO: I'm not sure for this one.
+                        scope_incremental = '<c-s>',
+                        node_decremental = '<c-backspace>',
+                    },
                 },
-                indent = {
-                    enable = true,
-                }
+                textobjects = {
+                    select = {
+                        enable = true,
+                        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+                        keymaps = {
+                            -- You can use the capture groups defined in textobjects.scm
+                            ['af'] = '@function.outer',
+                            ['if'] = '@function.inner',
+                            ['ac'] = '@class.outer',
+                            ['ic'] = '@class.inner',
+                        },
+                    },
+                    move = {
+                        enable = true,
+                        set_jumps = true, -- whether to set jumps in the jumplist
+                        goto_next_start = {
+                            [']m'] = '@function.outer',
+                            [']]'] = '@class.outer',
+                        },
+                        goto_next_end = {
+                            [']M'] = '@function.outer',
+                            [']['] = '@class.outer',
+                        },
+                        goto_previous_start = {
+                            ['[m'] = '@function.outer',
+                            ['[['] = '@class.outer',
+                        },
+                        goto_previous_end = {
+                            ['[M'] = '@function.outer',
+                            ['[]'] = '@class.outer',
+                        },
+                    },
+                    swap = {
+                        enable = true,
+                        swap_next = {
+                            ['<leader>a'] = '@parameter.inner',
+                        },
+                        swap_previous = {
+                            ['<leader>A'] = '@parameter.inner',
+                        },
+                    },
+                },
             }
             vim.opt.foldmethod = "indent"
             vim.opt.foldexpr = "nvim_treesitter#foldexpr()"
         end,
     })
- 
+
     use 'kyazdani42/nvim-web-devicons'
- 
+
     use ({ 'kyazdani42/nvim-tree.lua',
         config = function()
             require("nvim-tree").setup()
         end,
     })
- 
+
     use 'nvim-lua/plenary.nvim'
     use 'nvim-telescope/telescope.nvim'
     use 'neovim/nvim-lspconfig'
@@ -94,73 +143,73 @@ require('packer').startup(function(use)
             local cmp = require'cmp'
 
             cmp.setup({
-              snippet = {
-                -- REQUIRED - you must specify a snippet engine
-                expand = function(args)
-                   require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
-                end,
-              },
-              window = {
-                completion = cmp.config.window.bordered(),
-                -- documentation = cmp.config.window.bordered(),
-              },
-              mapping = cmp.mapping.preset.insert({
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                ['<C-Space>'] = cmp.mapping.complete(),
-                ['<C-e>'] = cmp.mapping.abort(),
-                ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
-              }),
-              sources = cmp.config.sources({
-                { name = 'nvim_lsp' },
-                { name = 'luasnip' }, -- For luasnip users.
-              }, {
-                { name = 'buffer' },
-              })
+                snippet = {
+                    -- REQUIRED - you must specify a snippet engine
+                    expand = function(args)
+                        require('luasnip').lsp_expand(args.body) -- For `luasnip` users.
+                    end,
+                },
+                window = {
+                    completion = cmp.config.window.bordered(),
+                    -- documentation = cmp.config.window.bordered(),
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }), -- Accept currently selected item. Set `select` to `false` to only confirm explicitly selected items.
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' }, -- For luasnip users.
+                }, {
+                        { name = 'buffer' },
+                    })
             })
 
             -- Set configuration for specific filetype.
             cmp.setup.filetype('gitcommit', {
-              sources = cmp.config.sources({
-                { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
-              }, {
-                { name = 'buffer' },
-              })
+                sources = cmp.config.sources({
+                    { name = 'cmp_git' }, -- You can specify the `cmp_git` source if you were installed it.
+                }, {
+                        { name = 'buffer' },
+                    })
             })
 
             -- Use buffer source for `/` (if you enabled `native_menu`, this won't work anymore).
             cmp.setup.cmdline('/', {
-              mapping = cmp.mapping.preset.cmdline(),
-              sources = {
-                { name = 'buffer' }
-              }
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = {
+                    { name = 'buffer' }
+                }
             })
 
             -- Use cmdline & path source for ':' (if you enabled `native_menu`, this won't work anymore).
             cmp.setup.cmdline(':', {
-              mapping = cmp.mapping.preset.cmdline(),
-              sources = cmp.config.sources({
-                { name = 'path' }
-              }, {
-                { name = 'cmdline' }
-              })
+                mapping = cmp.mapping.preset.cmdline(),
+                sources = cmp.config.sources({
+                    { name = 'path' }
+                }, {
+                        { name = 'cmdline' }
+                    })
             })
 
             -- Setup lspconfig.
             local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
             require('lspconfig')['clangd'].setup {
-              capabilities = capabilities
+                capabilities = capabilities
             }
 
             require('lspconfig')['texlab'].setup {
-              capabilities = capabilities,
-              settings = {
-                latex = {
-                  build = {
-                    onSave = true;
-                  }
+                capabilities = capabilities,
+                settings = {
+                    latex = {
+                        build = {
+                            onSave = true;
+                        }
+                    }
                 }
-              }
             }
         end
     })
@@ -174,7 +223,7 @@ require('packer').startup(function(use)
                 load = {
                     ["core.defaults"] = {}
                 }
-        }
+            }
         end,
     })
 
@@ -192,4 +241,17 @@ require('packer').startup(function(use)
         end
     })
 
+    use ({'lukas-reineke/indent-blankline.nvim',
+        config = function() 
+            vim.opt.list = true
+            vim.opt.listchars:append "eol:↴"
+            require('indent_blankline').setup {
+                char = '┊',
+                show_trailing_blankline_indent = true,
+                show_current_context = true,
+                show_current_context_start = true,
+                show_end_of_line = true,
+            }
+        end
+    })
 end)
