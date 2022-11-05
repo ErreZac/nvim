c5946e8df48bb81b8dae863f295089f684b9071b
ErreZac <raphael.zaccone@unige.it>
2022-11-05 22:31:04 +0100

    fixed update_capabilities it was deprecated

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -232,7 +232,7 @@ require('packer').startup(function(use)
             })
 
             -- Setup lspconfig.
-            local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+            local capabilities = require('cmp_nvim_lsp').default_capabilities(vim.lsp.protocol.make_client_capabilities())
             require('lspconfig')['clangd'].setup {
                 capabilities = capabilities
             }
